c24ba0ddb4af2827b3fe9a232595d0fc5be20860
joao-artl <jalxpinheiro@gmail.com>
2025-05-09 14:54:30 -0300

    feat(tests): criando teste do hello world

--- src/hello/tests.py
+++ src/hello/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class HelloViewTest(TestCase):
+    def test_hello_view_returns_200(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_hello_view_returns_expected_content(self):
+        response = self.client.get('/')
+        self.assertContains(response, "Hello, World!")
