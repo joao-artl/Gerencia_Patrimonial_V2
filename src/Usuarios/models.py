d14f936a684684faa466d1fdf688a45fb65088c9
joao-artl <jalxpinheiro@gmail.com>
2025-06-26 18:49:03 -0300

    feat(django): adiciona UNIQUE ao email do usuario

--- src/Usuarios/models.py
+++ src/Usuarios/models.py
@@ -8,7 +8,7 @@ TIPO_USUARIO_CHOICES = [
 
 class Usuario(models.Model):
     cpf = models.CharField(max_length=14, unique=True, verbose_name="CPF")
-    email = models.EmailField(max_length=255, verbose_name="E-mail")
+    email = models.EmailField(max_length=255, unique=True, verbose_name="E-mail")
     nome = models.CharField(max_length=255, verbose_name="Nome Completo")
     senha = models.CharField(max_length=128, verbose_name="Senha")
     tipo_usuario = models.CharField(max_length=20, choices=TIPO_USUARIO_CHOICES, verbose_name="Tipo de Usuário")
