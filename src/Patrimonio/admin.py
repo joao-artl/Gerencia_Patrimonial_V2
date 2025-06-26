85e71e996223aea15c46fc90660efb6c5b4f375d
joao-artl <jalxpinheiro@gmail.com>
2025-06-26 15:30:15 -0300

    feat(django): adiciona CRUDs inciais para Imobiliario, Endereco, Utilitario, Veiculo

--- src/Patrimonio/admin.py
+++ src/Patrimonio/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Imobiliario, Utilitario, Veiculo
 
-# Register your models here.
+admin.site.register(Imobiliario)
+admin.site.register(Utilitario)
+admin.site.register(Veiculo)
