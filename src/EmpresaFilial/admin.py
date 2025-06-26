56be30264323256aa388e8acf2eaf17bb87cdf8f
joao-artl <jalxpinheiro@gmail.com>
2025-06-26 17:37:21 -0300

    feat(django): adiciona Empresa e Filial ao CRUD inicial

--- src/EmpresaFilial/admin.py
+++ src/EmpresaFilial/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Endereco
+from .models import Endereco, Empresa, Filial
 
 admin.site.register(Endereco)
+admin.site.register(Empresa)
+admin.site.register(Filial)
