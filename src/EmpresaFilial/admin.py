0ff5c8508a5525648c1271625e1f8900e84485fd
joao-artl <jalxpinheiro@gmail.com>
2025-06-26 18:07:42 -0300

    feat(django): adiciona classe Gerencia no CRUD inicial

--- src/EmpresaFilial/admin.py
+++ src/EmpresaFilial/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Endereco, Empresa, Filial
+from .models import Endereco, Empresa, Filial, Gerencia
 
 admin.site.register(Endereco)
 admin.site.register(Empresa)
 admin.site.register(Filial)
+admin.site.register(Gerencia)
