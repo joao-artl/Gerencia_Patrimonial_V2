463c1c44d36e927eaadd3221f5b6623d32cb748c
joao-artl <jalxpinheiro@gmail.com>
2025-06-27 23:18:09 -0300

    feat(listagem): cria views para listar Patrimonio, Empresa, Filial e Usuario

--- src/Usuarios/views.py
+++ src/Usuarios/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Usuario
 
-# Create your views here.
+def listar_empresa(request, usuario_id):
+    usuario = get_object_or_404(Usuario, id=usuario_id)
+    empresa_administrada = usuario.empresa_administrada.all()
+    return render(request, 'listar_empresa_administrada.html', {'usuario': usuario, 'empresa_administrada': empresa_administrada})
