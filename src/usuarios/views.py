01868001c68e49dacb5bc01b54c501a406de257c
joao-artl <jalxpinheiro@gmail.com>
2025-06-29 12:55:53 -0300

    feat(django): cria endpoint da api para o app usuarios

--- src/usuarios/views.py
+++ src/usuarios/views.py
@@ -1,5 +1,12 @@
+from rest_framework import viewsets
 from django.shortcuts import render, get_object_or_404
 from .models import Usuario
+from .serializers import UsuarioSerializer
+
+
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all().order_by('-cpf')
+    serializer_class = UsuarioSerializer
 
 def listar_empresa(request, usuario_id):
     usuario = get_object_or_404(Usuario, id=usuario_id)
