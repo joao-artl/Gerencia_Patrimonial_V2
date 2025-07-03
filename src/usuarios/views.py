07e6256f92cde147307baabd48eab563e490f0b4
joao-artl <jalxpinheiro@gmail.com>
2025-07-03 19:29:18 -0300

    feat(django): adiciona listagem de empresas e filiais para o gestor

--- src/usuarios/views.py
+++ src/usuarios/views.py
@@ -1,8 +1,12 @@
 from rest_framework import viewsets, permissions
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from .models import Usuario
-from empresa_filial.models import Gerencia
+from empresa_filial.models import Gerencia, Filial
 from .serializers import UsuarioSerializer, GerenciaSerializer
+from empresa_filial.serializers import EmpresaSerializer, FilialSerializer 
 from .permissions import IsOwner, UserCreationPermission, IsEmployeeOfThisBranchOrManager, IsGestor
+
 
 class UsuarioViewSet(viewsets.ModelViewSet):
 
@@ -11,14 +15,32 @@ class UsuarioViewSet(viewsets.ModelViewSet):
     
     def get_permissions(self):
 
-        if self.action == 'create':
-            permission_classes = [UserCreationPermission]
-        elif self.action in ['update', 'partial_update', 'destroy']:
+        if self.action in ['update', 'partial_update', 'destroy']:
             permission_classes = [permissions.IsAuthenticated, IsOwner]
+        elif self.action in ['empresas_gerenciadas', 'filiais_acessiveis']:
+            permission_classes = [permissions.IsAuthenticated, IsOwner, IsGestor]
+        elif self.action == 'create':
+            permission_classes = [UserCreationPermission]
         else: 
             permission_classes = [permissions.IsAdminUser]
             
         return [permission() for permission in permission_classes]
+
+    @action(detail=True, methods=['get'], url_path='empresas')
+    def empresas_gerenciadas(self, request, pk=None):
+        gestor = self.get_object()
+        empresas = gestor.empresa_administrada.all()
+        serializer = EmpresaSerializer(empresas, many=True)
+        return Response(serializer.data)
+
+    @action(detail=True, methods=['get'], url_path='filiais')
+    def filiais_acessiveis(self, request, pk=None):
+
+        gestor = self.get_object()
+        ids_empresas_gerenciadas = gestor.empresa_administrada.values_list('id', flat=True)
+        filiais = Filial.objects.filter(empresa_matriz_id__in=ids_empresas_gerenciadas)
+        serializer = FilialSerializer(filiais, many=True)
+        return Response(serializer.data)
 
 
 class FuncionarioViewSet(viewsets.ReadOnlyModelViewSet):
@@ -32,8 +54,8 @@ class FuncionarioViewSet(viewsets.ReadOnlyModelViewSet):
             filial_associada_id=self.kwargs['filial_pk']
         )
 
-
 class GerenciaViewSet(viewsets.ModelViewSet):
+
     serializer_class = GerenciaSerializer
     permission_classes = [permissions.IsAuthenticated, IsGestor]
 
