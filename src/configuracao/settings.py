80084f13e081e4b47f5b55825df5685bd1d31e95
joao-artl <jalxpinheiro@gmail.com>
2025-07-17 23:13:55 -0300

    fix(api): remove "/" do endereço do deploy

--- src/configuracao/settings.py
+++ src/configuracao/settings.py
@@ -146,5 +146,5 @@ SPECTACULAR_SETTINGS = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://gerencia-patrimonial.vercel.app/",
+    "https://gerencia-patrimonial.vercel.app",
 ]
