c4685ca005ce2e216f20f5d9e12f1a1a88c603cf
joao-artl <jalxpinheiro@gmail.com>
2025-07-17 23:09:11 -0300

    feat(api): conecta a api com o deploy do vercel

--- src/configuracao/settings.py
+++ src/configuracao/settings.py
@@ -146,5 +146,5 @@ SPECTACULAR_SETTINGS = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://possivelfrontend.com", # ambiente de produção
+    "https://gerencia-patrimonial.vercel.app/",
 ]
